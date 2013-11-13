e477707b78ac21d9102462b7e72c6cc14878fe15
Pornpen Wonganusorn <pornpen.wong@gmail.com>
2013-11-13 09:42:08 +0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RubymineTest::Application.config.secret_key_base = 'c217e8ae4704b8491ccfa9477e0ddc7893b8372953089159ed86dee7c5916aa6b76575733f0e9dd70864eac3d40f51552a01f3c95e7108ae1546cae4ad167e97'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RubymineTest::Application.config.session_store :cookie_store, key: '_rubymine-test_session'
